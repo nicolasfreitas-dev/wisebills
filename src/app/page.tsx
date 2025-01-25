e57c82f4d6399986620569424a7832983140babc
Nicolas <nicolasfpdev@gmail.com>
2025-01-24 23:31:47 -0300

    feat: create TableTransactions component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Amount from "@/components/Amount";
-// import Table from "@/components/Table";
+import TableTransactions from "@/components/TableTransactions";
+import ValueContainer from "@/components/ValueContainer";
 import { TrendingDown, Wallet } from "lucide-react";
 
 export default function Home() {
     return (
         <main className="mx-10 pb-32">
             <h1 className="font-bold text-3xl mt-10">Dashboard</h1>
-            <Amount background="bg-bg-cash">
+            <ValueContainer background="bg-bg-cash">
                 <div className="flex items-center gap-3 ml-6 mt-7">
                     <div className="size-14 flex items-center justify-center bg-bg-primary rounded-xl">
                         <Wallet />
@@ -16,8 +16,8 @@ export default function Home() {
                 <span className="font-bold text-4xl ml-6 mt-7">
                     R$ 3.900,00
                 </span>
-            </Amount>
-            <Amount>
+            </ValueContainer>
+            <ValueContainer>
                 <div className="flex items-center gap-3 ml-6 mt-7">
                     <div className="size-14 flex items-center justify-center bg-bg-expense-color rounded-xl">
                         <TrendingDown className="text-expense-color" />
@@ -27,7 +27,7 @@ export default function Home() {
                 <span className="font-bold text-4xl ml-6 mt-7">
                     R$ 3.900,00
                 </span>
-            </Amount>
+            </ValueContainer>
             <div className="size-full border border-border-color rounded-[20px] flex flex-col mt-4 px-8">
                 <div className="h-32 flex items-center justify-between">
                     <h3 className="text-3xl font-bold">Transações</h3>
@@ -36,14 +36,10 @@ export default function Home() {
                 <hr className="border-border-color" />
                 <div>
                     <h4 className="text-2xl font-bold mt-7 mb-5">Pendentes</h4>
-                    {/* <Debito />
-                    <Debito />
-                    <Debito />
-                    <Debito />
-                    <Debito /> */}
+                    <TableTransactions />
                     <h4 className="text-2xl font-bold mt-7 mb-5">Concluídas</h4>
-                    {/* <Debito />
-                    <Debito /> */}
+                    <TableTransactions />
+                    
                 </div>
             </div>
         </main>
---
+++ src/components/TableTransactions/index.tsx
@@ -0,0 +1,19 @@
+import { Table, TableBody, TableCell, TableRow } from "../ui/table";
+
+export default function TableTransactions() {
+    return (
+        <Table>
+            <TableBody>
+                <TableRow className="flex items-center justify-between">
+                    <TableCell className="text-lg font-bold">15/01</TableCell>
+                    <TableCell className="flex flex-col text-lg">
+                        <span className=" font-bold">Computador</span>
+                        <span>Parcela 3 de 3</span>
+                    </TableCell>
+                    <TableCell className="text-lg font-bold">R$ 900,00</TableCell>
+                    <input type="checkbox" />
+                </TableRow>
+            </TableBody>
+        </Table>
+    );
+}
