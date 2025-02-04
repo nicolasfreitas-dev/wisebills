a1b4ac095f6c133509388a51b7b3300fb4099d1e
Nicolas <nicolasfpdev@gmail.com>
2025-02-04 13:02:37 -0300

    feat: create SelectField Component to use on TransactionForm Modal

---
+++ src/components/SelectField/index.tsx
@@ -0,0 +1,28 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+
+interface SelectFieldProps {
+    placeholder: string,
+    selectItem: string[],
+
+}
+
+export default function SelectField({ placeholder, selectItem }: SelectFieldProps) {
+    return (
+        <Select>
+            <SelectTrigger className="h-14 rounded-[1.2rem] border-border-color mb-8 text-xl">
+                <SelectValue placeholder={placeholder} />
+            </SelectTrigger>
+            <SelectContent className="bg-bg-secondary border-border-color">
+                {
+                    selectItem.map((item, index) => {
+                        return (
+                            <SelectItem key={index} className="cursor-pointer text-xl" value={item}>
+                                {item}
+                            </SelectItem>
+                        )
+                    })
+                }
+            </SelectContent>
+        </Select>
+    );
+};
