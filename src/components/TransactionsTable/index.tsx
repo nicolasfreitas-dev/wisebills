060b50a8d4565d84bffceb1e5254ab5024be6ee7
nicolasfreitas <nicolasfpdev@gmail.com>
2025-02-20 15:54:01 -0300

    refactor: Add conditional render elements

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -11,20 +11,38 @@ export default function TransactionsTable({ transactions }: TransactionTableProp
             <TableBody>
                 {transactions.map((transaction, index) => {
                     return (
-                        <TableRow key={index} className="flex items-center justify-between">
+                        <TableRow
+                            key={index}
+                            className="flex items-center justify-between border-0"
+                        >
                             <TableCell className="text-lg font-bold">
-                                {transaction.date.toLocaleDateString('pt-BR')}
+                                {transaction.date
+                                    .toLocaleDateString("pt-BR")
+                                    .split("/")
+                                    .slice(0, 2)
+                                    .join("/")}
                             </TableCell>
-                            <TableCell className="flex flex-col text-lg">
-                                <span className=" font-bold">{transaction.title}</span>
-                                <span>Parcela {transaction.parcel}</span>
+                            <TableCell className="w-full max-w-[12rem] flex flex-col items-start justify-start text-lg">
+                                <span className="font-bold">
+                                    {transaction.title}
+                                </span>
+                                <span>
+                                    {transaction.paymentMethod === "Cartão"
+                                        ? `Parcela X de ${transaction.parcel}`
+                                        : transaction.paymentMethod}
+                                </span>
                             </TableCell>
-                            <TableCell className="text-lg font-bold">
-                                {transaction.amount}
+                            <TableCell
+                                className={`text-lg font-bold ${
+                                    transaction.type === "Gasto"
+                                        ? "text-expense-color"
+                                        : "text-green-detail"
+                                }`}
+                            >
+                                {`R$ ${transaction.amount}`}
                             </TableCell>
                         </TableRow>
-
-                    )
+                    );
                 })
                 }
                 {transactions.length === 0 && (
