3378d69b41d00fe06ee55dbf8ff77ee495757a3c
nicolasfreitas <nicolasfpdev@gmail.com>
2025-03-10 20:41:35 -0300

    refactor: adjust on padding and margin of AmountContainer component

--- src/components/AmountContainer/index.tsx
+++ src/components/AmountContainer/index.tsx
@@ -7,7 +7,7 @@ interface AmountProps {
 
 export default function AmountContainer({ children, background }: AmountProps) {
     return (
-        <div className={`w-full h-56 border border-border-color rounded-[20px] flex flex-col gap-3 items-start mt-6 ${background || ""}`}>
+        <div className={`w-full h-56 border border-border-color rounded-[20px] flex flex-col gap-3 items-start justify-center py-6 px-8 mt-6 ${background || ""}`}>
             {children}
         </div>
     )
