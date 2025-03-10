5b3f6129acbfe58ca048248f1ea88d27f469c6f1
nicolasfreitas <nicolasfpdev@gmail.com>
2025-03-10 20:43:05 -0300

    refactor: change project name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SobraCash",
+  title: "Wisebills",
   description: "Aplicativo de gestão financeira",
 }
 
