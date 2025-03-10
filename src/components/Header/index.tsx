5a27a5ded9d40ba7238b634b208a0e959589a649
nicolasfreitas <nicolasfpdev@gmail.com>
2025-03-10 20:40:52 -0300

    refactor: implement navbar on Header component and change logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,20 @@
+import { CircleUserRound } from "lucide-react";
+import Navbar from "../Navbar";
+
 export default function Header() {
     return (
-        <header className="w-full h-20 flex items-center gap-4 border-b-[1px] border-b-border-color px-10">
-            <span className="text-4xl font-bold">SobraCash</span>
+        <header className="w-full h-24 flex items-center justify-between gap-4 border-b-[1px] border-b-border-color px-10">
+            <span className="text-4xl font-bold">Wisebills</span>
+            <Navbar>
+                <nav className={`h-24 flex items-center gap-10 list-none border-b-[1px] border-b-border-color ${window.innerWidth <= 640 ? "hidden" : ""}`}>
+                    <li className="cursor-pointer p-6">Visão geral</li>
+                    <li className="cursor-pointer p-6">Detalhes</li>
+                    <li className="cursor-pointer p-6">Concluído</li>
+                </nav>
+            </Navbar>
+            <div>
+                <CircleUserRound />
+            </div>
         </header>
-    )
+    );
 };
