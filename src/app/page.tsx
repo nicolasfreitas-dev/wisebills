b137a3f2e101a7a2967688778bad663a2431815b
Nicolas <nicolasfpdev@gmail.com>
2025-03-14 13:54:24 -0300

    refactor: descentralize components of CalculateBalance to ensure code is more clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,21 @@
 
 import { useState } from "react";
 import TransactionsTable from "@/components/TransactionsTable";
-import AmountContainer from "@/components/AmountContainer";
 import Header from "@/components/Header";
 import Modal from "@/components/Modal";
 import {
-    ArrowUpDown,
     ChartColumnIcon,
     CheckIcon,
-    EyeIcon,
-    EyeClosedIcon,
     HouseIcon,
     PlusIcon,
-    TrendingDown,
-    Wallet,
 } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import { transaction } from "@/components/TransactionForm";
 import { z } from "zod";
 import Navbar from "@/components/Navbar";
 import { useResize } from "@/hooks/useResize";
+import Amount from "@/components/Amount";
+import Expenses from "@/components/Expenses";
+import Reserved from "@/components/Reserved";
 
 export default function Home() {
     const [isModalOpen, setIsModalOpen] = useState(false)
@@ -49,6 +45,7 @@ export default function Home() {
     const calculateBalance = () => {
         let balance = 0
         let expenses = 0
+        let reserved = 0
 
         transactions.forEach((transaction) => {
             const amount = parseFloat(
@@ -57,64 +54,35 @@ export default function Home() {
 
             if (transaction.type === "Depósito") {
                 balance += amount
-            } else {
+            } else if (transaction.type === "Gasto") {
                 balance -= amount
                 expenses += amount
+            } else {
+                reserved += amount
             }
 
-            if (transaction.type === "Gasto" && balance < 0) {
+            if (transaction.type === "Gasto" && balance < 0 || transaction.type === "Reserva" && balance < 0) {
                 balance = 0
             }
         })
 
-        return { balance, expenses }
+        return { balance, expenses, reserved }
     };
 
-    const { balance, expenses } = calculateBalance();
+    const { balance, expenses, reserved } = calculateBalance();
 
     return (
         <>
             <Header />
-            <main className="px-10 pb-32">
-                <AmountContainer background="bg-bg-cash">
-                    <div className="flex items-center gap-3">
-                        <div className="size-14 flex items-center justify-center bg-bg-primary rounded-xl">
-                            <Wallet />
-                        </div>
-                        <h3 className="text-2xl text-gray-detail">Saldo</h3>
+            <main className="md:flex md:gap-10 px-10 pb-32">
+                <div className="md:w-[60%] md:flex md:flex-col md:gap-4">
+                    <Amount balance={balance} windowSize={windowSize} hideAmount={hideAmount} handleHideAmount={handleHideAmount} handleModalOpen={handleModalOpen} />
+                    <div className="md:flex md:gap-10">
+                        <Reserved reserved={reserved}  />
+                        <Expenses expenses={expenses} />
                     </div>
-                    <div className="w-full flex items-center justify-between">
-                        <div className="flex items-center gap-5">
-                            <span className="font-bold text-4xl">
-                                {hideAmount ? "R$ " + balance.toFixed(2) : "R$ ••••"}
-                            </span>
-                            {
-                                hideAmount 
-                                ? <EyeIcon className="cursor-pointer" onClick={handleHideAmount} />
-                                : <EyeClosedIcon className="cursor-pointer" onClick={handleHideAmount} />
-                            }
-                        </div>
-                        <Button
-                            className={`bg-green-detail mt-3 rounded-[20px] px-8 py-6 text-xl ${windowSize < 768 ? "hidden" : ""}`}
-                            onClick={handleModalOpen}
-                        >
-                            <span>Adicionar transação</span>
-                            <ArrowUpDown />
-                        </Button>
-                    </div>
-                </AmountContainer>
-                <AmountContainer>
-                    <div className="flex items-center gap-3">
-                        <div className="size-14 flex items-center justify-center bg-bg-expense-color rounded-xl">
-                            <TrendingDown className="text-expense-color" />
-                        </div>
-                        <h3 className="text-2xl text-gray-detail">Despesas</h3>
-                    </div>
-                    <span className="font-bold text-4xl">
-                        {`R$ ${expenses.toFixed(2)}`}
-                    </span>
-                </AmountContainer>
-                <div className="size-full border border-border-color rounded-[20px] flex flex-col mt-4 px-8">
+                </div>
+                <div className="w-full md:w-[40%] border border-border-color rounded-[20px] flex flex-col mt-4 px-8">
                     <div className="h-32 flex items-center justify-between">
                         <h3 className="text-3xl font-bold">Transações</h3>
                         <span className="text-xl font-bold cursor-pointer">
@@ -139,14 +107,14 @@ export default function Home() {
                         <li className="p-6 cursor-pointer">
                             <HouseIcon />
                         </li>
+                        <li className="p-6 cursor-pointer">
+                            <ChartColumnIcon />
+                        </li>
                         <li
                             className="p-6 cursor-pointer"
                             onClick={handleModalOpen}
                         >
                             <PlusIcon />
-                        </li>
-                        <li className="p-6 cursor-pointer">
-                            <ChartColumnIcon />
                         </li>
                         <li className="p-6 cursor-pointer">
                             <CheckIcon />
