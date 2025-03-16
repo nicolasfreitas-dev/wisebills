c701e8e268ed5b0033fb544ab3b22499863279bd
nicolasfreitas <nicolasfpdev@gmail.com>
2025-03-16 15:43:40 -0300

    feat: install Zustand to manage global state of the project and create transactionStore to deal with transactions states

---
+++ src/store/transactions.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand"
+import { z } from "zod"
+import { transaction } from "@/components/TransactionForm"
+
+type transactionType = z.infer<typeof transaction>
+
+interface useTransaction {
+    transactions: transactionType[];
+    completedTransactions: transactionType[];
+    addTransaction: (transaction: transactionType) => void
+    addCompletedTransaction: (transaction: transactionType) => void
+    markAsCompleted: (id: number) => void
+}
+
+export const useTransactionStore = create<useTransaction>((set) => ({
+    transactions: [],
+    completedTransactions: [],
+    addTransaction: (transaction) => set((state) => ({
+        transactions: [...state.transactions, transaction]
+    })),
+    addCompletedTransaction: (transaction) => set((state) => ({
+    completedTransactions: [...state.completedTransactions, transaction]
+    })),
+    markAsCompleted: (id) => set((state) => {
+        const isCompleted = state.transactions.find((data) => data.id === id)
+
+        if (!isCompleted) return state;
+
+        return {
+            transactions: state.transactions.filter((data) => data.id !== id),
+            completedTransactions: [...state.completedTransactions, isCompleted]
+        }
+    })
+}))
