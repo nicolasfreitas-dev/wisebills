89d42739d3f335fccabce331012a33deed4b98cd
Nicolas Freitas <nicolasfpdev@gmail.com>
2025-04-13 13:08:51 -0300

    refactor: remove unnecessary navbar duplicated component

--- src/components/MobileNavbar/index.tsx
+++ src/components/MobileNavbar/index.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { HouseIcon, ChartColumnIcon, PlusIcon, CheckIcon, TrendingDown } from "lucide-react";
-import Navbar from "../Navbar";
 import { useModalStore } from "@/store/modal";
 import Link from "next/link";
 
@@ -9,27 +8,25 @@ export default function MobileNavbar() {
     const { setIsOpen } = useModalStore()
     
     return (
-        <Navbar>
-            <nav className="md:hidden w-full h-24 fixed bottom-0 left-0 flex items-center justify-evenly list-none bg-primary border-t-[1px] border-t-witheWithOpacity">
-                <Link href="/" className="p-6 cursor-pointer">
-                    <HouseIcon />
-                </Link>
-                <Link href="/pages/details" className="p-6 cursor-pointer">
-                    <ChartColumnIcon />
-                </Link>
-                <li
-                    className="p-6 cursor-pointer"
-                    onClick={() => setIsOpen(true)}
-                >
-                    <PlusIcon />
-                </li>
-                <Link href="/pages/pending">
-                    <TrendingDown />
-                </Link>
-                <Link href="/pages/completed" className="p-6 cursor-pointer">
-                    <CheckIcon />
-                </Link>
-            </nav>
-        </Navbar>
+        <nav className="md:hidden w-full h-24 fixed bottom-0 left-0 flex items-center justify-evenly list-none bg-primary border-t-[1px] border-t-witheWithOpacity">
+            <Link href="/" className="p-6 cursor-pointer">
+                <HouseIcon />
+            </Link>
+            <Link href="/pages/details" className="p-6 cursor-pointer">
+                <ChartColumnIcon />
+            </Link>
+            <li
+                className="p-6 cursor-pointer"
+                onClick={() => setIsOpen(true)}
+            >
+                <PlusIcon />
+            </li>
+            <Link href="/pages/pending">
+                <TrendingDown />
+            </Link>
+            <Link href="/pages/completed" className="p-6 cursor-pointer">
+                <CheckIcon />
+            </Link>
+        </nav>
     );
 };
