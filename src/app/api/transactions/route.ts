7bc1c6387c978373e712fa6e80be86a34a34f85c
Nicolas Freitas <nicolasfpdev@gmail.com>
2025-05-10 13:26:55 -0300

    feat: create transaction routes

---
+++ src/app/api/transactions/route.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@/generated/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function Post(req: NextRequest) {
+    const body = await req.json();
+
+    const { title, amount, type, paymentMethod, parcel, category, date } = body;
+
+    try {
+        const transaction = await prisma.transaction.create({
+            data: {
+                title,
+                amount: parseFloat(amount),
+                type,
+                paymentMethod ,
+                parcel: parcel ? parseInt(parcel) : undefined,
+                category,
+                date: new Date(date)
+            }
+        });
+
+        return NextResponse.json(transaction);
+
+    } catch (error) {
+        console.error(error);
+
+        return NextResponse.json({ error: "Erro ao criar transação" }, { status: 500 });
+    }
+}
