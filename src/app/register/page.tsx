f31294121b7c50211159b1d27b517aff3b4dd080
Nicolas <nicolasfpdev@gmail.com>
2025-05-12 16:12:11 -0300

    feat: add eslitnignore and remove unnused import from register/page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm, useWatch } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
