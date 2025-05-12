523954b50f512f3368daf19824640c442423d793
Nicolas <nicolasfpdev@gmail.com>
2025-05-12 16:25:57 -0300

    refactor: ensure .gitignore work correctly, and add more specific eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
     ...compat.extends("next/core-web-vitals", "next/typescript"),
     {
+        files: ["**/*.js", "**/*.ts", "**/*.tsx"],
         ignores: [
             "node_modules/",
             ".next/",
