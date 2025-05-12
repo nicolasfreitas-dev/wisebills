00d70ed12ffce80101b00ed268fc004c4edc4563
Nicolas <nicolasfpdev@gmail.com>
2025-05-12 15:50:48 -0300

    fix: ensure next js identify correctly the method post on route.ts

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function Post(req: NextRequest) {
+export async function POST(req: NextRequest) {
     const body = await req.json();
 
     const { title, amount, type, paymentMethod, parcel, category, date } = body;
