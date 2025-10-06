b189e1d5bd0cb0be07f8e23aa0ca23c9c1077063
Nicolas Freitas <nicolasfpdev@gmail.com>
2025-10-06 16:42:09 -0300

    feat: create update method CRUD

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -1,3 +1,4 @@
+import { transactionSchema } from "@/lib/validations/transaction-schema";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -27,5 +28,36 @@ export async function POST(req: NextRequest) {
         console.error(error);
 
         return NextResponse.json({ error: "Erro ao criar transação" }, { status: 500 });
+    }
+}
+
+export async function GET() {
+    try {
+        const transactions = await prisma.transaction.findMany({orderBy: {id: 'desc'}});
+
+        return NextResponse.json(transactions)
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+export async function UPDATE(req: Request, { params }: {params : {id: string}}) {
+    try {
+        const data = await req.json();
+        const response = transactionSchema.parse(data);
+        const transaction = await prisma.transaction.update({
+            where: { id: Number(params.id) },
+            data: {
+                ...response,
+                amount: parseFloat(response.amount),
+                parcel: response.parcel ? parseInt(response.parcel) : undefined,
+            },
+        });
+
+        return NextResponse.json(transaction)
+
+    } catch (error) {
+        return NextResponse.json({error: (error as Error).message || "Erro ao atualizar transação"}, {status: 400})
     }
 }
